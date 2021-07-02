5b4644f5dc769cc7a9a183281fe015f2ee8f8cb4
vinicius.oliveira <vinicius.oliveira@sequor.com.br>
2021-07-01 22:43:37 -0300

    chore: remove files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DicesModule } from './dices/dices.module';
+import { DicesController } from './dices/dices.controller';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://mongo:27017/nest', {
     useNewUrlParser: true, 
     useUnifiedTopology: true
-  })],
-  controllers: [AppController],
+  }), DicesModule],
+  controllers: [AppController, DicesController],
   providers: [AppService],
 })
 export class AppModule {}
